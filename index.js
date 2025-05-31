bd0632eaf6d3921e41a2591db9043d1e3d3168f4
ravib7 <ravibchaudhari7@gmail.com>
2025-05-31 16:06:27 +0530

    updated backend with frontend integration

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
+const path = require("path");
 require("dotenv").config()
 
 const app = express()
 app.use(express.json())
-app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+app.use(express.static("dist"))
+app.use(cors({
+    origin: "https://ravi-portfolio-xo6k.onrender.com",
+    credentials: true
+}))
 
 app.use("/api/contact", require("./routes/contact.route"))
 
 app.use("*", (req, res) => {
-    res.status(404).json({ message: "Resource Not Found" })
+    res.sendFile(path.join(__dirname, "dist", "index.html"))
 })
 
 app.use((err, req, res, next) => {
